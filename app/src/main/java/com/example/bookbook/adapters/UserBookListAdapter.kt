ebe8996ada1f5a3faa3b82cb3be262451ab0bc60
Gabriel Firmino Gadelha <gfg2@uhwc08.windows.cin.ufpe.br>
2019-12-14 09:47:03 -0300

    Adding UserBookList layout

--- app/src/main/java/com/example/bookbook/adapters/UserBookListAdapter.kt
+++ app/src/main/java/com/example/bookbook/adapters/UserBookListAdapter.kt
@@ -5,18 +5,25 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bookbook.R
 import com.example.bookbook.consts.Consts
 import com.example.bookbook.entities.Book
 import com.example.bookbook.views.activities.BookDetailsActivity
-import kotlinx.android.synthetic.main.recycler_fav_book_cell.view.*
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.recycler_book_list_cell.view.*
+import kotlinx.android.synthetic.main.recycler_book_search_cell.view.*
 
 class UserBookListAdapter( private val ctx: Context, private val list: List<Book>): RecyclerView.Adapter<UserBookListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(ctx).inflate(R.layout.recycler_fav_book_cell, parent, false)
+        val view = LayoutInflater.from(ctx).inflate(R.layout.recycler_book_search_cell, parent, false)
+
+        view.btn_add_book_fav_list.visibility = View.INVISIBLE
+        view.btn_add_book_wish_list.visibility = View.INVISIBLE
+
         return ViewHolder(view)
     }
 
@@ -26,7 +33,7 @@ class UserBookListAdapter( private val ctx: Context, private val list: List<Book
         val book = list[position]
 
         holder.title?.text  = book.title
-        holder.author?.text = book.author
+        Picasso.get().load(book.image).into(holder.image)
 
         holder.itemView.setOnClickListener {
             val intent = Intent(ctx, BookDetailsActivity::class.java)
@@ -37,7 +44,7 @@ class UserBookListAdapter( private val ctx: Context, private val list: List<Book
 
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        var title:  TextView? = view.txt_fav_book_title
-        var author: TextView? = view.txt_fav_book_authors
+        val title: TextView? = view.txt_book_title
+        val image: ImageView? = view.img_book_cover
     }
 }
